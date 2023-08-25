86f925840a4855d3c383f73f2b04369c57387dee
shauryaMalhotra27 <shaurya@orientaloutsourcing.com>
2023-08-25 15:43:49 +0530

    added spinner

--- app/src/main/java/com/example/newregiform/registerPage.java
+++ app/src/main/java/com/example/newregiform/registerPage.java
@@ -3,19 +3,29 @@ package com.example.newregiform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.text.DateFormatSymbols;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class registerPage extends AppCompatActivity {
 
 
-    EditText nameRegiET, ageRegiET, genderRegiET, emailRegiET;
-    TextView goToPassBtn, goToLoginBtnRegi  ;
+    EditText nameRegiET, ageRegiET, emailRegiET;
+    Spinner  genderRegiET;
+    TextView goToPassBtn, goToLoginBtnRegi;
+
+    String[] gender;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +44,7 @@ public class registerPage extends AppCompatActivity {
         goToLoginBtnRegi = findViewById(R.id.goToLoginBtnRegi);
 
 
-
-
+        populateSpinner();
 
         goToLoginBtnRegi.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,22 +60,51 @@ public class registerPage extends AppCompatActivity {
 
                 String name1 = nameRegiET.getText().toString().trim();
                 String age1 = ageRegiET.getText().toString().trim();
-                String gender1 = genderRegiET.getText().toString().trim();
+                String gender1 = genderRegiET.getSelectedItem().toString();
                 String email1 = emailRegiET.getText().toString().trim();
 
-                Intent i = new Intent(getApplicationContext(), passwordPage.class);
+                if (name1.isEmpty() || age1.isEmpty() || gender1.isEmpty() || email1.isEmpty()) {
 
-                i.putExtra("NAME", name1);
-                i.putExtra("AGE", age1);
-                i.putExtra("GENDER", gender1);
-                i.putExtra("EMAIL", email1);
+                    warnToast();
 
-                startActivity(i);
+                } else {
+                    Intent i = new Intent(getApplicationContext(), passwordPage.class);
 
+                    i.putExtra("NAME", name1);
+                    i.putExtra("AGE", age1);
+                    i.putExtra("GENDER", gender1);
+                    i.putExtra("EMAIL", email1);
 
+                    startActivity(i);
+
+                }
 
             }
         });
+
+
+    }
+
+    public void warnToast() {
+        Toast toastWarn = new Toast(getApplicationContext());
+        View warnView = getLayoutInflater().inflate(R.layout.custom_toast_yellow, (ViewGroup) findViewById(R.id.warningLayout));
+        toastWarn.setView(warnView);
+        TextView textMsg = warnView.findViewById(R.id.textYellow);
+        textMsg.setText("FILL ALL DETAILS");
+        toastWarn.setDuration(Toast.LENGTH_SHORT);
+        toastWarn.setGravity(Gravity.TOP, 0, 30);
+        toastWarn.show();
+
+    }
+
+    private void populateSpinner() {
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.gender, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        genderRegiET.setAdapter(adapter);
+
 
 
     }
